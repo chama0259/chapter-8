1a56eac687fb06d971b833a52130bf73c0f064c3
chama0259 <chamaaadev0259@gmail.com>
2026-03-20 15:47:00 +0900

    バックエンド(API)実装

---
+++ app/api/admin/categories/[id]/route.ts
@@ -0,0 +1,106 @@
+import { prisma } from "@/app/_libs/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+//カテゴリー詳細APIのレスポンスの型
+export type CategoryShowResponse = {
+  category: {
+    id: number;
+    name: string;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+};
+
+//カテゴリー更新時のリクエストbodyの型
+export type UpdateCategoryRequestBody = {
+  name: string;
+};
+
+export const GET = async (
+  _request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  //paramsからid取り出す
+  const { id } = await params;
+
+  try {
+    //idをもとにCategoriesをDBに取得
+    const category = await prisma.category.findUnique({
+      where: {
+        //Urlからparamsから取得したidは文字列なので忘れない
+        id: parseInt(id),
+      },
+    });
+
+    if (!category) {
+      return NextResponse.json(
+        { message: "記事が見つかりません" },
+        { status: 404 },
+      );
+    }
+
+    //レスポンスを返す
+    return NextResponse.json<CategoryShowResponse>(
+      { category },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    }
+  }
+};
+
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  //idをparamsから取得
+  const { id } = await params;
+
+  //リクエストのbody取り出す
+  const { name }: UpdateCategoryRequestBody = await request.json();
+
+  try {
+    //idを指定してカテゴリーテーブル更新
+    await prisma.category.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name,
+      },
+    });
+
+    //レスポンスを返す
+    return NextResponse.json({ messge: "OK" }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    }
+  }
+};
+
+export const DELETE = async (
+  _request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  //idをparamsから取得
+  const { id } = await params;
+
+  try {
+    //idを指定してcategoryを削除
+    await prisma.category.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    //レスポンスを返す
+    return NextResponse.json({ message: "OK" }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    }
+  }
+};
---
+++ app/api/admin/posts/route.ts
@@ -0,0 +1,98 @@
+import { prisma } from "@/app/_libs/prisma";
+import { NextResponse } from "next/server";
+
+//GET用型定義
+export type PostIndexResponse = {
+  posts: {
+    id: number;
+    title: string;
+    content: string;
+    thumbnailUrl: string;
+    createdAt: Date;
+    upsatedAt: Date;
+    postCategories: {
+      category: {
+        id: number;
+        name: string;
+      };
+    }[];
+  }[];
+};
+
+//投稿作成時に送られてくるリクエストのbodyの型
+export type CreatePostRequestBody = {
+  title: string;
+  content: string;
+  categories: { id: number }[];
+  thumbnailUrl: string;
+};
+
+//投稿作成APIのレスポンスの型（投稿作成後にフロント側に教えるもの）
+export type CreatePostResponse = {
+  id: number;
+};
+
+export const GET = async () => {
+  try {
+    const posts = await prisma.post.findMany({
+      include: {
+        postCategories: {
+          include: {
+            category: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json({ posts }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+};
+
+//
+export const POST = async (request: Request) => {
+  try {
+    //リクエストのbodyを取得
+    const body: CreatePostRequestBody = await request.json();
+
+    //bodyの中からtitle,content,categories,thumbnailUrlを取り出す
+    const { title, content, categories, thumbnailUrl } = body;
+
+    //投稿をDBに生成
+    const data = await prisma.post.create({
+      data: {
+        title,
+        content,
+        thumbnailUrl,
+      },
+    });
+
+    //記事とカテゴリーの中間テーブルのレコードをDBに生成
+    //本来複数同時生成には、createManyというメソッドがあるが、sqliteではcreateManyが使えないので、for文１つずつ実施
+    for (const category of categories) {
+      await prisma.postCategory.create({
+        data: {
+          categoryId: category.id,
+          postId: data.id,
+        },
+      });
+    }
+
+    //レスポンスを返す
+    return NextResponse.json<CreatePostResponse>({ id: data.id });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    }
+  }
+};
