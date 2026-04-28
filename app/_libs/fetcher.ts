ed0782d414bf14018bf779f976222cf3b3a24ce0
chama0259 <chamaaadev0259@gmail.com>
2026-04-28 15:22:43 +0900

    fetcherを配列以外も受け入れるように修正

--- app/_libs/fetcher.ts
+++ app/_libs/fetcher.ts
@@ -1,12 +1,16 @@
 //SWRで使う共通のfetcher
 //第一引数はURL、第二引数は（あれば）Authorization Token
-export const fetcher = async ([url, token]: [string, string | null]) => {
+export const fetcher = async (args: string | [string, string | null]) => {
+  // 引数が配列なら [url, token] に分解、文字列なら url だけ取り出す
+  const [url, token] = Array.isArray(args) ? args : [args, null];
   const headers: HeadersInit = {
     "Content-Type": "application/json",
   };
+
   if (token) {
     headers["Authorization"] = token;
   }
+
   const res = await fetch(url, { headers });
 
   if (!res.ok) {
